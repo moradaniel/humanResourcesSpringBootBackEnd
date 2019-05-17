007be75f5b7d58523e45d2f9473ae87c2fd5531e
moradaniel <moradaniel@gmail.com>
2019-05-17 20:24:10 -0300

    Added Swagger support

--- README.md
+++ README.md
@@ -8,7 +8,11 @@
 
 Rest:
 
--Swagger
+### Swagger
+```
+http://localhost:8082/humanResources-rest/swagger-ui.html#/
+```
+
 -Integration tests
 -Stateless API with JWT
 
@@ -19,12 +23,59 @@ Features
 -Logging requests with CommonsRequestLoggingFilter
 
 
-Testing 
 
-Integration tests
+### Run a jar file from console
+```
+$java -cp build/libs/humanResources.jar org.humanResources.service.Main Reader
+```
 
+### Install Oracle dependencies
+```
+mvn install:install-file -Dfile=ojdbc7.jar  -DgroupId=com.oracle -DartifactId=ojdbc7 -Dversion=12.1.0.1 -Dpackaging=jar
+```
+
+## Testing
+https://www.petrikainulainen.net/programming/maven/integration-testing-with-maven/
+https://www.javaworld.com/article/2074569/core-java/unit-and-integration-tests-with-maven-and-junit-categories.html
+
+### Unit testing
+
+$mvn clean test -Pdevelopment
+
+### Integration testing
+$mvn clean verify -Pintegration-test
+
+
+## Maven commands
+
+### Build humanresources-core
+
+To install all projects:
+
+```
+/parent/$mvn clean install
+```
+
+To avoid running the tests
+
+/parent/$mvn clean install -DskipTests
+
+For Reinke use the "reinke" profile defined in parent-pom
+
+/path/to/ranch/projects/parent-pom$ mvn clean install -DskipTests -Preinke
+
+
+
+
+
+TODO
+Add AWS support with Localstack
+https://www.youtube.com/watch?v=FOzAdoxdnSc
 
 
 Resources
+
 https://www.baeldung.com/spring-security-login-angular
 https://www.baeldung.com/spring-http-logging
+
+https://grokonez.com/spring-framework/spring-security/angular-spring-boot-jwt-authentication-example-angular-6-spring-security-mysql-full-stack-part-1-overview-and-architecture
--- humanResources-rest/src/main/java/com/baeldung/springbootsecurityrest/security/WebSecurityConfig.java
+++ humanResources-rest/src/main/java/com/baeldung/springbootsecurityrest/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -59,12 +60,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }*/
-    
+
+
+    /*
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }*/
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.cors().and().csrf().disable().
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
+                //Begin Swagger
+                .antMatchers("/v2/api-docs","/swagger-ui.html","/configuration/**", "/swagger*/**", "/webjars/**")
+                .permitAll()
+                //End Swagger
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
