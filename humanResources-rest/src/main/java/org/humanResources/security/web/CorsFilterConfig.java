8af2f364f2aaef06c9fd3d387575d60b2e5756c3
moradaniel <moradaniel@gmail.com>
2019-06-08 20:50:08 -0300

    Enabling CORS

---
+++ humanResources-rest/src/main/java/org/humanResources/security/web/CorsFilterConfig.java
@@ -0,0 +1,30 @@
+package org.humanResources.security.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+/**
+ * https://chariotsolutions.com/blog/post/angular-2-spring-boot-jwt-cors_part1/
+ */
+@Configuration
+public class CorsFilterConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
